642f7b2a18ad3fe48d12d0d7ced98ad622a31a73
Yoshiki Tsukada <y-tsukada0323@docomo.ne.jp>
2020-04-05 19:11:19 +0900

    Create ArticleCardContentView

--- QiitaMobile/Views/Components/ArticleCardCell.swift
+++ QiitaMobile/Views/Components/ArticleCardCell.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class ArticleCardCell: UICollectionViewCell, CollectionViewCellPresenter {
-    @IBOutlet weak var authorImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var newLabel: UILabel!
-    @IBOutlet weak var authorNameLabel: UILabel!
-    @IBOutlet weak var likesCountLabel: UILabel!
-    
-    static func estimatedSize(width: CGFloat) -> CGSize {
-        return .init(width: width, height: 100)
-    }
-    
-    func apply() {
-        authorImageView.backgroundColor = .lightGray
+    @IBOutlet weak var articleCardContentView: ArticleCardContentView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
         layer.masksToBounds = false
         layer.shadowOffset = CGSize(width: 0, height: 4)
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOpacity = 0.2
         layer.shadowRadius = 8
+    }
+
+    static func estimatedSize(width: CGFloat) -> CGSize {
+        return .init(width: width, height: 100)
+    }
+
+    func apply() {
     }
 }
---
+++ QiitaMobile/Views/Components/ArticleCardContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ArticleCardContentView.swift
+//  QiitaMobile
+//
+//  Created by Tsukada Yoshiki on 2020/04/05.
+//
+
+import UIKit
+
+class ArticleCardContentView: UIView {
+    @IBOutlet weak var authorImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var newLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var likesCountLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        authorImageView.backgroundColor = .lightGray
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        guard let view = UINib(nibName: String(describing: type(of: self)), bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Could not load Nibfile.")
+        }
+
+        view.frame = bounds
+        addSubview(view)
+    }
+
+    func loadView() {
+    }
+}
